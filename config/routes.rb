149cb8c75f0b34f2a36d73f5ed21a98d473a440b
soraya de lisa <soraya.delisa@gmail.com>
2020-10-21 12:36:32 +0200

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
